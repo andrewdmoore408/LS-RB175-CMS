20235bbb8a9d3331d50e961ebc37a6532d945bcf
Andrew Moore <andrewdmoore84@gmail.com>
2021-08-11 01:33:17 -0400

    Add ability to display files in a directory and link to their plaintext contents

--- cms.rb
+++ cms.rb
@@ -2,6 +2,25 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
 
+root = File.expand_path("..", __FILE__)
+data_dir = "/public/data"
+
+helpers do
+  def only_filename(file)
+    File.basename(file)
+  end
+end
+
 get "/" do
-  "Getting started."
+  @files = Dir.glob(root + data_dir + "/*")
+
+  erb :files
+end
+
+get "/view/:filename" do
+  file = File.read(root + data_dir + "/" + params[:filename])
+
+  status 200
+  headers "Content-Type" => "text/plain"
+  body file
 end
